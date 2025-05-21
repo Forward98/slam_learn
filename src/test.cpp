0a6dae48bcc2e2e9660453f145f7b276f92aa207
Forward98 <m15256916478@163.com>
2025-05-22 00:20:47 +0800

    eigen库测试

--- src/test.cpp
+++ src/test.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <Eigen/Core>
 
 using namespace std;
 using namespace cv;
 
 int main() {
+    /*
+    // 测试opencv
     // 指定图片路径
     string path = "E:\\VScode_project\\slam_learn\\src\\img_0067.png";
     cv::Mat img = imread(path);  // 读取图像
@@ -16,6 +19,26 @@ int main() {
 
     // 等待按键按下，关闭窗口
     waitKey(0);
+    */
+
+    // 测试Eigen
+    // 创建一个简单的矩阵
+    Eigen::MatrixXd matrix(2, 2);
+    matrix << 1, 2,
+              3, 4;
+
+    // 创建一个向量
+    Eigen::VectorXd vector(2);
+    vector << 1, 2;
+
+    // 矩阵与向量相乘
+    Eigen::VectorXd result = matrix * vector;
+
+    // 输出结果
+    std::cout << "Matrix:\n" << matrix << std::endl << std::endl;
+    std::cout << "Vector:\n" << vector << std::endl << std::endl;
+    std::cout << "Result of matrix * vector:\n" << result << std::endl;
+
 
     return 0;
 }
