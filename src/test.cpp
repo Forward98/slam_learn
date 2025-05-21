baf9ae5423bfa61dc6cca5485807923180a107c6
Forward98 <m15256916478@163.com>
2025-05-22 00:39:29 +0800

    lec2_code

--- src/test.cpp
+++ src/test.cpp
@@ -1,44 +1,44 @@
-#include <iostream>
-#include <opencv2/opencv.hpp>
-#include <Eigen/Core>
+// #include <iostream>
+// #include <opencv2/opencv.hpp>
+// #include <Eigen/Core>
 
-using namespace std;
-using namespace cv;
+// using namespace std;
+// using namespace cv;
 
-int main() {
-    /*
-    // 测试opencv
-    // 指定图片路径
-    string path = "E:\\VScode_project\\slam_learn\\src\\img_0067.png";
-    cv::Mat img = imread(path);  // 读取图像
+// int main() {
+//     /*
+//     // 测试opencv
+//     // 指定图片路径
+//     string path = "E:\\VScode_project\\slam_learn\\src\\img_0067.png";
+//     cv::Mat img = imread(path);  // 读取图像
 
-    // 显示图像
-    imshow("Display Image", img);
-    cout << "Image size: " << img.size() << endl;  // 输出图像大小
-    // imshow("Display Image", test);
+//     // 显示图像
+//     imshow("Display Image", img);
+//     cout << "Image size: " << img.size() << endl;  // 输出图像大小
+//     // imshow("Display Image", test);
 
-    // 等待按键按下，关闭窗口
-    waitKey(0);
-    */
+//     // 等待按键按下，关闭窗口
+//     waitKey(0);
+//     */
 
-    // 测试Eigen
-    // 创建一个简单的矩阵
-    Eigen::MatrixXd matrix(2, 2);
-    matrix << 1, 2,
-              3, 4;
+//     // 测试Eigen
+//     // 创建一个简单的矩阵
+//     Eigen::MatrixXd matrix(2, 2);
+//     matrix << 1, 2,
+//               3, 4;
 
-    // 创建一个向量
-    Eigen::VectorXd vector(2);
-    vector << 1, 2;
+//     // 创建一个向量
+//     Eigen::VectorXd vector(2);
+//     vector << 1, 2;
 
-    // 矩阵与向量相乘
-    Eigen::VectorXd result = matrix * vector;
+//     // 矩阵与向量相乘
+//     Eigen::VectorXd result = matrix * vector;
 
-    // 输出结果
-    std::cout << "Matrix:\n" << matrix << std::endl << std::endl;
-    std::cout << "Vector:\n" << vector << std::endl << std::endl;
-    std::cout << "Result of matrix * vector:\n" << result << std::endl;
+//     // 输出结果
+//     std::cout << "Matrix:\n" << matrix << std::endl << std::endl;
+//     std::cout << "Vector:\n" << vector << std::endl << std::endl;
+//     std::cout << "Result of matrix * vector:\n" << result << std::endl;
 
 
-    return 0;
-}
+//     return 0;
+// }
